ce2143a39bce1f18bede08e3a84c921e33d0be76
Luiz Silva Cunha <luiz.silva.cunha@everis.com>
2020-03-25 16:55:45 -0300

    implementa swagger e adiciona gitignore

---
+++ src/main/java/com/nice/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.nice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+@Configuration @EnableSwagger2 public class SwaggerConfig {
+
+    @Bean public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build();
+    }
+
+    private ApiInfo metaInfo() {
+
+        ApiInfo apiInfo =
+            new ApiInfo("Real Sucos API REST", "API REST Real Sucos", "1.0", "Terms of Service",
+                new Contact("", "r", ""), "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>());
+
+        return apiInfo;
+    }
+}
--- src/main/java/com/nice/main/controller/PedidoController.java
+++ src/main/java/com/nice/main/controller/PedidoController.java
@@ -1,27 +1,18 @@
 package com.nice.main.controller;
 
-import java.util.Date;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.List;
-import java.util.Locale;
-
-import javax.validation.Valid;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.nice.model.ItemPedido;
 import com.nice.model.PedidoData;
 import com.nice.service.PedidoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 @RestController
 public class PedidoController {
